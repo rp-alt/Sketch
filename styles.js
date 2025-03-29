07e6923b91d340c4db21c2f83546a3476677f420
Rahul Prasad <rahulprd000@gmail.com>
2025-03-29 20:24:13 +0100

    Main part done

--- styles.js
+++ styles.js
@@ -1,7 +1,29 @@
 const container = document.querySelector("#container");
-for (let i = 1; i <= 264; i++) {
+let squares = prompt("How many squares");
+for (let i = 1; i <= squares*squares; i++) {
     const box = document.createElement("div");
     box.classList.add("box");
     box.textContent = `This is box ${i}`;
     container.appendChild(box);
+}
+document.body.addEventListener('mouseover', e => {
+    let box = e.target.closest('.box');
+    if (!box) { return; }
+    box.style.backgroundColor = generateAllColors();
+  });
+  
+  /** document.body.addEventListener('mouseout', e => {
+    let box = e.target.closest('.box');
+    if (!box) { return; }
+    box.style.backgroundColor = '';
+  }); **/
+
+  function generateAllColors() {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+    
+  }
+document.body.addEventListener('click')
+
+function Reset() {
+    location.reload();
 }
